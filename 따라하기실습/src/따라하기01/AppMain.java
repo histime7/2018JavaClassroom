d08354d29f709000c89ece216696c6a3acaa8401
HISTIME <HISTIME@YSH>
2018-08-21 21:03:34 +0900

    윤상현

---
+++ 따라하기실습/src/따라하기01/AppMain.java
@@ -0,0 +1,10 @@
+package 따라하기01;
+
+class AppMain {
+	public static void main(String[] args) {
+		System.out.println("난 main() 함수!");
+		System.out.println("자바 프로그램은 나로부터 시작해!");
+		
+		new Class01();
+	}
+}
